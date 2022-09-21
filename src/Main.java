836661637bff9e98b1ed55e3b27fd97cc4385c2d
JustynaBolek <justyna.bolek@gmail.com>
2022-09-21 20:37:08 +0200

    added new method addition

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Calculator cal1 = new Calculator();
+        System.out.println(cal1.addition(4,6));
 
     }
 }
