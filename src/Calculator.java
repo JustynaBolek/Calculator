f391e7b7a6dfa02dae44b82d0f401625188491bf
JustynaBolek <justyna.bolek@gmail.com>
2022-09-21 20:49:31 +0200

    added new method multiplication and division

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,4 +6,10 @@ public class Calculator {
     public int subtraction(int x, int y){
         return x-y;
     }
+    public int multiplication(int x, int y){
+        return x*y;
+    }
+    public int division(int x, int y){
+        return x/y;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         Calculator cal1 = new Calculator();
         System.out.println(cal1.addition(4,6));
         System.out.println(cal1.subtraction(10,3));
+        System.out.println(cal1.multiplication(2,2));
+        System.out.println(cal1.division(30,5));
     }
 }
